4051d474b4fe0b43c99a565c1c40cc37d1eb0c29
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-25 17:24:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,12 @@
 def join_nested_strings(src)
-  no_num = []
   row_index = 0
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
       src.keep_if { |obj| (obj.is_a? String) }
-      no_num += src[row_index][element_index]
       src.map(&:join).join(' ')
       element_index += 1
     end
     row_index
   end
-  no_num
 end
